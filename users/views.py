59847601f09b258cd45766ecab3995bc5c64f79f
elBukhara <shakhzodgiazov@gmail.com>
2024-08-04 13:28:06 +0300

    profile interface added

--- users/views.py
+++ users/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from blog.models import Article
 
 # Create your views here.
+
+def profile_view(request):
+    
+    published_articles = Article.objects.filter(author=request.user, status='published')
+    drafted_articles = Article.objects.filter(author=request.user, status='draft')
+    
+    context = {
+        'published_articles': published_articles,
+        'drafted_articles': drafted_articles
+    }
+    
+    return render(request, 'users/profile.html', context)
